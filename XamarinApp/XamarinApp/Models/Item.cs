7f0af4babb7b4eb3220961804b7d5d381bd3ee0e
SOFTYDREAM <ka.aissani@gmail.com>
2017-12-30 21:32:02 +0100

    Commit for extract

--- XamarinApp/XamarinApp/Models/Item.cs
+++ XamarinApp/XamarinApp/Models/Item.cs
@@ -5,8 +5,10 @@ namespace XamarinApp
 {
     public class Item : RealmObject
     {
+
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
     }
 }
